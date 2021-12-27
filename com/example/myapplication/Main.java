dcd6cb0d3fb0505d3f4b997963f1b00d1f704c14
imnixan <86918436+imnixan@users.noreply.github.com>
2021-12-27 03:50:01 +0300

    Delete Main.java

--- com/example/myapplication/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-       for (int i = 0; i < 10; i++){
-           new FakeClient(i);
-       }
-    }
-}
